20caad05b1f7ff25424e88a7b5733b1aab01336e
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-29 07:09:08 +0800

    feat: add token generation methods
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -3,7 +3,24 @@ class Stash < ApplicationRecord
 
   before_create :generate_uniq_uuid
 
+  def generate_uniq_token
+    token = generate_token
+    while Stash.find_by_token(token).present?
+      token = generate_token
+    end
+    Kredis.integer("share:#{token}", expires_in: 1.hour).value = self.id
+    token
+  end
+
+  def self.find_by_token(token)
+    find_by(id: Kredis.integer("share:#{token}").value)
+  end
+
   private
+
+  def generate_token
+    SecureRandom.hex(16)
+  end
 
   def generate_uuid
     SecureRandom.uuid
