94a839123c8f60e785f2ee9a5929bb19effff6eb
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-29 05:57:34 +0800

    feat: generate uniq uuid on stash create
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -1,3 +1,21 @@
 class Stash < ApplicationRecord
   has_one_attached :attachment
+
+  before_create :generate_uniq_uuid
+
+  private
+
+  def generate_uuid
+    SecureRandom.uuid
+  end
+
+  def generate_uniq_uuid
+    unless self.uuid?
+      uuid = generate_uuid
+      while Stash.exists?(uuid: uuid)
+        uuid = generate_uuid
+      end
+      self.uuid = uuid
+    end
+  end
 end
