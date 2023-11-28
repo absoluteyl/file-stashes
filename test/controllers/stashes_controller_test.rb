d5e5860ff2bb1038c8191ee9924b72696f6d3adc
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-28 12:21:04 +0800

    test: stashes table header
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- test/controllers/stashes_controller_test.rb
+++ test/controllers/stashes_controller_test.rb
@@ -4,6 +4,19 @@ class StashesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "index page displays stash table header" do
+    get stashes_url
+    assert_response :success
+    assert_select 'table' do
+      assert_select 'thead tr', count: 1
+      assert_select 'thead tr th', text: 'ID'
+      assert_select 'thead tr th', text: 'Download'
+      assert_select 'thead tr th', text: 'Delete'
+      assert_select 'thead tr th', text: 'Created at'
+      assert_select 'thead tr th', text: 'Updated at'
+    end
+  end
+
   test "should create stash" do
     # Create a file fixture to upload
     attachment = fixture_file_upload('stash_attachment.txt', 'text/plain')
