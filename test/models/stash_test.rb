f51e4ed8327a005b8ff0f7e8113cf4d51d26bea6
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-29 18:31:07 +0800

    test: stash must have attachment
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- test/models/stash_test.rb
+++ test/models/stash_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StashTest < ActiveSupport::TestCase
+  test "should not be valid without attachment" do
+    stash = Stash.new
+    assert_not stash.valid?
+  end
+
   test "should be valid" do
     stash = FactoryBot.create(:stash)
     assert stash.valid?
