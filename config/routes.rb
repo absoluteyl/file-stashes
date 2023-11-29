428880091c42f1b614e7b9600c5d5cf2ccdf52c3
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-29 19:44:41 +0800

    feat: change root path to new stash path
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "stashes#index"
+  root "stashes#new"
 
   # Defines the routes for the Stash resource
-  resources :stashes, only: [:index, :create, :destroy]
+  resources :stashes, only: [:index, :new, :create, :destroy]
 end
