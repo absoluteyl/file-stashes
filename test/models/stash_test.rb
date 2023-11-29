76eb1d1d062f8de75c59ef7375a3b6a9ae53f1c2
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-29 21:59:23 +0800

    test: stash token behaviors
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- test/models/stash_test.rb
+++ test/models/stash_test.rb
@@ -21,4 +21,24 @@ class StashTest < ActiveSupport::TestCase
     stash2 = FactoryBot.create(:stash)
     assert stash1.uuid != stash2.uuid
   end
+
+  test "should generate a token" do
+    stash = FactoryBot.create(:stash)
+    token = stash.generate_uniq_token
+    assert token == stash.tokens.first
+  end
+
+  test "should generate a uniq token" do
+    stash = FactoryBot.create(:stash)
+    token1 = stash.generate_uniq_token
+    token2 = stash.generate_uniq_token
+    assert stash.tokens.count == 2
+    assert token1 != token2
+  end
+
+  test "should find stash by token" do
+    stash = FactoryBot.create(:stash)
+    token = stash.generate_uniq_token
+    assert stash == Stash.find_by_token(token)
+  end
 end
