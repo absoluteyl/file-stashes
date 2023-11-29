8010d31947c8873e82529e55b707056f447f9d1e
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-29 18:30:46 +0800

    refactor: use factory to create stash
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- test/models/stash_test.rb
+++ test/models/stash_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class StashTest < ActiveSupport::TestCase
   test "should be valid" do
-    stash = Stash.new
-    stash.attachment.attach(io: File.open('test/fixtures/files/stash_attachment.txt'), filename: 'stash_attachment.txt', content_type: 'text/plain')
+    stash = FactoryBot.create(:stash)
     assert stash.valid?
   end
 end
