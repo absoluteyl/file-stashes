f91ca8569e61800241f6e40abb818eb5b53ee32f
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-11-29 18:32:01 +0800

    test: stash must have a unique uuid
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- test/models/stash_test.rb
+++ test/models/stash_test.rb
@@ -10,4 +10,15 @@ class StashTest < ActiveSupport::TestCase
     stash = FactoryBot.create(:stash)
     assert stash.valid?
   end
+
+  test "should generate a uuid" do
+    stash = FactoryBot.create(:stash)
+    assert stash.uuid.present?
+  end
+
+  test "should generate a unique uuid" do
+    stash1 = FactoryBot.create(:stash)
+    stash2 = FactoryBot.create(:stash)
+    assert stash1.uuid != stash2.uuid
+  end
 end
