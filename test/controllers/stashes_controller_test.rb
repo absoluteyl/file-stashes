cd874e47a5326dfc238e62d4cde27dd250815a97
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-12-02 20:15:25 +0800

    test: update to h2
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- test/controllers/stashes_controller_test.rb
+++ test/controllers/stashes_controller_test.rb
@@ -3,7 +3,7 @@ class StashesControllerTest < ActionDispatch::IntegrationTest
   test "should get stash new when accessing root url" do
     get root_url
     assert_response :success
-    assert_select 'h1', text: 'Upload a file'
+    assert_select 'h2', text: 'Upload a file'
     assert_select 'form input[type=file]', count: 1
     assert_select 'form input[type=submit]', count: 1
   end
